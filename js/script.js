da9f179bd77c95b54ef3709f057d1bb903de7e79
ehsan <ehsantahamii@gmail.com>
2024-01-21 18:44:55 +0330

    commit #25

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ $("#closeMenu").click(function () {
 // dark / light
 $("#dark-btn").click(function () {
   $(
-    "body ,footer, nav, article,.reservation,.guid-dropdown,.nav-container-mobile,swiper-slide "
+    "body ,footer, nav, article,.reservation,.guid-dropdown,.nav-container-mobile,swiper-slide,.blog-card ,.sample-card"
   ).toggleClass("dark-active");
   $("h1 , h2, h3,h4,p , a, .arrow-vector , .tool-box button").css(
     "color",
@@ -42,7 +42,7 @@ $("#dark-btn").click(function () {
 });
 $("#light-btn").click(function () {
   $(
-    "body ,footer, nav ,article,.reservation,.guid-dropdown,.nav-container-mobile,swiper-slide"
+    "body ,footer, nav ,article,.reservation,.guid-dropdown,.nav-container-mobile,swiper-slide,.blog-card ,.sample-card"
   ).removeClass("dark-active");
   $("h1 , h2, h3,h4 ,p , a, .arrow-vector , .tool-box button").css("color", "");
   $(".arrow-vector, .arrow-vector path , button path").css("fill", "#333333");
